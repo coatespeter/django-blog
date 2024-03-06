d4588f9636cf969dd9951bd967bb4e2c5db2827d
coatespeter <coatespeter@yahoo.com>
2024-03-06 04:43:54 +0000

    add in allowed hosts to try fix access

--- codestar/settings.py
+++ codestar/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-coatespeter-django-blog-l63gipitf9.us1.codeanyapp.com','.herokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['8000-coatespeter-django-blog-l63gipitf9.us1.codeanyapp.com','.herokuapp.com', 'localhost', '127.0.0.1', '[::1]']
 
 
 # Application definition
